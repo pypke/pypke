1e92deeac4d1a9c2e3d947b4bafcda6008de7cb4
ItsMrNatural <premkishore273@gmail.com>
2022-03-05 18:34:42 +0530

    fix: more logging listeners

--- cogs/_logging.py
+++ cogs/_logging.py
@@ -1,24 +1,29 @@
+from datetime import datetime
+
 import discord
 from discord.ext import commands
-from datetime import datetime
 
 # This Is Currently In Devlopment
 
 
-def get_log(guild):
-    possible_names = ["modlogs", "logs", "logging", "mod-logs", "log"]
-
-    for name in possible_names:
-        try:
-            log_channel = discord.utils.get(guild.channels, name=name)
-            return log_channel
-        except:
-            continue
-
-
-class LoggingCog(commands.Cog, description="This cog handles the logging of various events!"):
+class LoggingCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    def get_guild_log(self, guild):
+        data = self.bot.config.get(guild.id)
+        if data:
+            log_channel = data["log"]
+            return log_channel
+
+        possible_names = ["modlogs", "logs", "logging", "mod-logs", "log"]
+
+        for name in possible_names:
+            try:
+                log_channel = discord.utils.get(guild.channels, name=name)
+                return log_channel
+            except:
+                continue
 
     @commands.Cog.listener()
     async def on_member_ban(self, guild, member):
@@ -30,7 +35,7 @@ class LoggingCog(commands.Cog, description="This cog handles the logging of vari
                 color=discord.Color.red(),
                 timestamp=datetime.now(),
             )
-            logs = get_log(guild)
+            logs = self.get_guild_log(guild)
             await logs.send(embed=embed)
 
     @commands.Cog.listener()
@@ -43,8 +48,31 @@ class LoggingCog(commands.Cog, description="This cog handles the logging of vari
                 color=discord.Color.green(),
                 timestamp=datetime.now(),
             )
-            logs = get_log(guild)
+            logs = self.get_guild_log(guild)
             await logs.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        if before.nick != after.nick:
+            embed = discord.Embed(
+                title="nickname change",
+                description=f"**Old Nickname:** {before.nick}\n**New Nickname:** {after.nick}",
+                color=discord.Color.blue(),
+                timestamp=datetime.now(),
+            )
+            logs = self.get_guild_log(before.guild)
+            await logs.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.User):
+        embed = discord.Embed(
+            title="join",
+            description=f"**User:** {member.mention} **│** {member}\n**User ID:** {member.id}\n**User Age:** {member.created_at}",
+            color=discord.Color.green(),
+            timestamp=datetime.now(),
+        )
+        logs = self.get_guild_log(member.guild)
+        await logs.send(embed=embed)
 
 
 def setup(bot):
