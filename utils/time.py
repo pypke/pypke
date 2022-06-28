ca46eece9d9158ff2bc6ae4f79408dad5d939d07
MrNatural <76610619+ItsMrNatural@users.noreply.github.com>
2022-06-28 07:36:36 +0000

    fix: add TimeHumanizer

--- utils/time.py
+++ utils/time.py
@@ -45,3 +45,40 @@ class TimeConverter(commands.Converter):
                 raise commands.BadArgument(f"{key} is not a number!")
         return round(time)
 
+class TimeHumanizer:
+    """Converts seconds to human-readable format.
+    Args:
+        value (int): The value to convert
+    Raises:
+        commands.BadArgument: If time is negative.
+    """
+
+    def __new__(self, value: int):
+        if value <= 0:
+            raise commands.BadArgument(f"Time cannot be negative.")
+
+        duration = ""
+
+        minutes, seconds = divmod(value, 60)
+        hours, minutes = divmod(minutes, 60)
+        days, hours = divmod(hours, 24)
+        months, days = divmod(days, 30)
+
+        duration += f"{months} months, " if months > 0 else ""
+        duration += f"{days} days, " if days > 0 else ""
+        duration += f"{hours} hours, " if hours > 0 else ""
+        duration += f"{minutes} minutes, " if minutes > 0 else ""
+        duration += f"{seconds} seconds" if seconds > 0 else ""
+        duration = duration.strip(", ")
+        if months == 1:
+            duration = duration.replace("months", "month")
+        if days == 1:
+            duration = duration.replace("days", "day")
+        if hours == 1:
+            duration = duration.replace("hours", "hour")
+        if minutes == 1:
+            duration = duration.replace("minutes", "minute")
+        if seconds == 1:
+            duration = duration.replace("seconds", "second")
+
+        return duration
