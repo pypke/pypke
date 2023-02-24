5dca3897b133e81f83d215e06d32020380ac7657
ItsMrNatural <>
2023-02-24 11:30:59 +0000

    style: error handling file

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,12 +1,9 @@
-# from thefuzz import fuzz
 import logging
 import random
 import string
 
 import discord
 from discord.ext import commands
-
-# from dislash import Interactionbot, ContextMenuInteraction, ApplicationCommandError
 
 
 class ErrorsCog(commands.Cog):
@@ -16,74 +13,37 @@ class ErrorsCog(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
-            # cmd_list = self.bot.all_commands
-            # i = 0
-            # msg = f"That's not a valid command! Do `{ctx.prefix}help` for the list of commands.\nDid you meant one of these?\n"
-            # for cmd in cmd_list:
-            #     command = self.bot.get_command(cmd)
-            #     if not command.hidden == True:
-            #         ratio = fuzz.ratio(ctx.message.content, cmd)
-            #         if ratio >= 59:
-            #             i += 1
-            #             msg = msg + f"**{i}.** `{cmd}`\n"
-            #             if i >= 10:
-            #                 break
-            #         else:
-            #             continue
-            # if i != 0:
-            #     await ctx.send(msg, delete_after=7)
-            # else:
-            #     pass
-            pass
+            pass  # Don't do anything if command is not found
 
         elif isinstance(error, commands.NoPrivateMessage):
-            try:
-                await ctx.author.send(
-                    f"{ctx.command} can not be used in Private Messages."
-                )
-            except discord.HTTPException:
-                pass
+            await ctx.author.send(f"{ctx.command} can not be used in private messages.")
 
         elif isinstance(error, commands.MissingPermissions):
-            try:
-                await ctx.send(
-                    f"You are missing `{', '.join(error.missing_permissions)}` permission(s) to run this command.",
-                    delete_after=5,
-                )
-                await ctx.message.delete()
-            except Exception:
-                pass
+            missing_perms = ", ".join(error.missing_permissions)
+            await ctx.send(f"You are missing the following permissions to run this command: {missing_perms}", delete_after=5)
+            await ctx.message.delete()
 
         elif isinstance(error, commands.BotMissingPermissions):
-            try:
-                await ctx.send(
-                    f"Bot doesn't have the required `{', '.join(error.missing_permissions)}` permission(s).",
-                    delete_after=5,
-                )
-                await ctx.message.delete()
-            except Exception:
-                pass
+            missing_perms = ", ".join(error.missing_permissions)
+            await ctx.send(f"The bot doesn't have the following permissions: {missing_perms}", delete_after=5)
+            await ctx.message.delete()
 
         elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(
-                f"`{error.param.name}` is a required argument that is missing."
-            )
+            await ctx.send(f"You are missing a required argument: `{error.param.name}`")
+            # Uncomment the following line if you want to automatically invoke the help command for the current command
             # await ctx.invoke(self.bot.get_command("help"), command_or_module=ctx.command.qualified_name)
 
         elif isinstance(error, commands.NotOwner):
-            # await ctx.send("Lol, You should be owner of the bot to do this.")
-            pass
+            pass  # Don't do anything if user is not the owner of the bot
 
         elif isinstance(error, commands.UserNotFound):
-            await ctx.send(f'User "{error.argument}" doesn\'t exist. Try again!')
+            await ctx.send(f'The user "{error.argument}" does not exist. Please try again!')
 
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(
-                f'Member "{error.argument}" is not in this server or doesn\'t exist. Try again!'
-            )
+            await ctx.send(f'The member "{error.argument}" is not in this server or does not exist. Please try again!')
 
         elif isinstance(error, commands.ChannelNotFound):
-            await ctx.send(f'Channel "{error.argument}" not found. Try again!')
+            await ctx.send(f'The channel "{error.argument}" does not exist. Please try again!')
 
         elif isinstance(error, commands.BadArgument):
             await ctx.send(str(error))
@@ -92,18 +52,16 @@ class ErrorsCog(commands.Cog):
             await ctx.send(str(error))
 
         elif isinstance(error, commands.MaxConcurrencyReached):
-            await ctx.send(f"Woah, This command is already in progress.", delete_after=5)
+            await ctx.send(f"This command is already in progress.", delete_after=5)
 
         elif isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(
-                f"Chill! This command is on cooldown for `{round(error.retry_after)}` seconds."
-            )
+            await ctx.send(f"This command is on cooldown for `{round(error.retry_after)}` seconds.")
 
         elif isinstance(error, commands.CommandInvokeError):
             error_key = "".join(random.choices(string.hexdigits, k=10))
             embed = discord.Embed(
-                title="Error occured",
-                description=f"Looks like an error occured, pls consider reporting the error ID to the developers on the [support server](https://dsc.gg/pypke-support).",
+                title="Error Occurred",
+                description=f"An error occurred while executing the command. Please consider reporting the error ID to the developers on the [support server](https://dsc.gg/pypke-support).",
                 timestamp=discord.utils.utcnow(),
                 color=self.bot.colors["og_blurple"],
             )
